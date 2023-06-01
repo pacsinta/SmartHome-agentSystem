d92dee0d52aa195bf57bd1950a18de15179f1e80
bferi2001 <barnafeco@gmail.com>
2023-06-01 20:56:45 +0200

    ajtonyitas

--- src/agt/biztonsagErzekelo.asl
+++ src/agt/biztonsagErzekelo.asl
@@ -10,3 +10,14 @@
 
 +!start : true <- .print("hello world.").
 
++azonositasTrue <- .send(biztonsagErzekelo,tell,ajtonyitas);
+                    .print("azonositas sikeres").
+
++azonositasFalse <- .print("azonositas sikertelen").
+
++ajtonyitas <- .print("ajto kinyilt");
+                .wait(3000);
+                .send(biztonsagErzekelo,tell,ajtocsukas).
+                
+
++ajtocsukas <- .print("ajto becsukodott").
--- src/env/example/Env.java
+++ src/env/example/Env.java
@@ -3,18 +3,14 @@
 import jason.asSyntax.*;
 import jason.environment.*;
 import jason.asSyntax.parser.*;
-
 import java.util.logging.*;
-
+import java.lang.Math.*;
 public class Env extends Environment {
 
     private Logger logger = Logger.getLogger("smarthome."+Env.class.getName());
 
     private boolean tuzvan = false;
     private boolean oxigenhianyvan = false;
-
-    private boolean jogosultsagokElfogadva = false;
-    private boolean bejaratiajtoNyitva = false;
 
     /** Called before the MAS execution with the args informed in .mas2j */
     @Override
@@ -35,12 +31,18 @@ public class Env extends Environment {
             case "gyujtas" -> addPercept(Literal.parseLiteral("tuz"));
             case "gazszivargas" -> addPercept(Literal.parseLiteral("oxigenhiany"));
             case "tuzvan(1)" -> addPercept(Literal.parseLiteral("tuzvan(1)"));
+            case "mozgas" -> {
+                double rnd = Math.random();
+                    if(rnd>0.5){addPercept(Literal.parseLiteral("azonositasTrue"));}
+                    else{addPercept(Literal.parseLiteral("azonositasFalse"));}
+                }
+            case "ajtonyitas" -> addPercept(Literal.parseLiteral("ajtonyitas"));
+            case "ajtocsukas" -> {
+                    if(!tuzvan&&!oxigenhianyvan){
+                        addPercept(Literal.parseLiteral("ajtocsukas"));
+                    }
+                }
             default -> logger.info("executing: " + action + ", but not implemented!");
-        }
-
-
-        if(jogosultsagokElfogadva){
-            bejaratiajtoNyitva = true;
         }
 
 
